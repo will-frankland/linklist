4c616c608bff04b7aa21740d9c6412f7f3ba4e5a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-10-02 21:30:39 -0400

    setup useEffect to grab form input save to localstorage and use for redirect during signup

--- src/components/forms/HeroForm.jsx
+++ src/components/forms/HeroForm.jsx
@@ -1,8 +1,35 @@
-"use client"
+"use client";
+import { signIn } from "next-auth/react";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function HeroForm() {
+  useEffect(() => {
+    if (
+      'localStorage' in window
+      && window.localStorage.getItem('desiredUsername')
+    ) {
+      const username = window.localStorage.getItem('desiredUsername');
+      window.localStorage.removeItem('desiredUsername')
+      redirect('/account?username=' + username)
+    }
+  }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.querySelector("input");
+    const username = input.value;
+    if (username.length > 0) {
+      window.localStorage.setItem('desiredUsername', username)
+      await signIn("google", {
+      });
+    }
+  };
   return (
-    <form className="inline-flex items-center shadow-lg shadow-gray-700/20">
+    <form
+      onSubmit={handleSubmit}
+      className="inline-flex items-center shadow-lg shadow-gray-700/20"
+    >
       <span className="bg-white py-4 pl-4">linklist.to/</span>
       <input type="text" className="py-4" placeholder="username" />
       <button type="submit" className="bg-blue-500 text-white py-4 px-6">
